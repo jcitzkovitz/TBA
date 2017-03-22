4016a5ac95efa1c20dbdecf37cad3fe4483a3bfd
bijanshahriari <bijan.shahriari@mail.mcgill.ca>
2017-03-22 17:40:47 -0400

    launch test 4

--- src/tests/BallLauncher.java
+++ src/tests/BallLauncher.java
@@ -14,30 +14,28 @@ public class BallLauncher {
 	public void launch(){
 	//lift the ball
 
-	this.leftCatapultMotor.setAcceleration(10);
-	this.rightCatapultMotor.setAcceleration(10);
-	this.leftCatapultMotor.setSpeed(15);
-	this.rightCatapultMotor.setSpeed(15);
+	this.leftCatapultMotor.setSpeed(20);
+	this.rightCatapultMotor.setSpeed(20);
 	this.leftCatapultMotor.rotate(-40,true);
 	this.rightCatapultMotor.rotate(-40, false);
 	Sound.beep();
-	Button.waitForAnyPress();
+	
 	//launch the ball
 
-	this.leftCatapultMotor.setAcceleration(2000);
-	this.rightCatapultMotor.setAcceleration(2000);
-	this.leftCatapultMotor.setSpeed(2000);
-	this.rightCatapultMotor.setSpeed(2000);
-	this.leftCatapultMotor.rotate(-110, true);
-	this.rightCatapultMotor.rotate(-110,false);
+	this.leftCatapultMotor.setAcceleration(2200);
+	this.rightCatapultMotor.setAcceleration(2200);
+	this.leftCatapultMotor.setSpeed(2500);
+	this.rightCatapultMotor.setSpeed(2500);
+	this.leftCatapultMotor.rotate(-100, true);
+	this.rightCatapultMotor.rotate(-100,false);
 	Sound.twoBeeps();
 	//return to the initial position
 	this.leftCatapultMotor.setAcceleration(500);
 	this.rightCatapultMotor.setAcceleration(500);
 	this.leftCatapultMotor.setSpeed(90);
 	this.rightCatapultMotor.setSpeed(90);
-	this.leftCatapultMotor.rotate(150, true);
-	this.rightCatapultMotor.rotate(150, false);
+	this.leftCatapultMotor.rotate(140, true);
+	this.rightCatapultMotor.rotate(140, false);
 	Sound.twoBeeps();
 	
 	}
